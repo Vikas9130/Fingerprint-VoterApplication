a506fc07974f8e2818c8f396771cced0d49249ba
91913 <91913@JaTaDhariShiv>
2023-05-27 16:37:25 +0530

    User bean class added

---
+++ Fingerprint-Voter-Applicaiton/src/main/java/com/Model/User.java
@@ -0,0 +1,66 @@
+package com.Model;
+
+public class User {
+
+	private String name;
+	private String gender;
+	private String dob;
+	private String email;
+	private String address;
+	
+	public User() {
+		
+	}
+	public User(String name, String gender, String dob, String email, String address) {
+		this.name = name;
+		this.gender = gender;
+		this.dob = dob;
+		this.email = email;
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", gender=" + gender + ", dob=" + dob + ", email=" + email + ", address="
+				+ address + "]";
+	}
+	
+	
+	
+	
+
+	
+	
+	
+	
+
+}
