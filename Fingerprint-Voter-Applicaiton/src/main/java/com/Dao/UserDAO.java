1a360887660a1ddcea1dbd983d7845671a451586
91913 <91913@JaTaDhariShiv>
2023-05-27 21:00:50 +0530

    ps.setDate(7, date1) corrected

--- Fingerprint-Voter-Applicaiton/src/main/java/com/Dao/UserDAO.java
+++ Fingerprint-Voter-Applicaiton/src/main/java/com/Dao/UserDAO.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
+import java.sql.Date;
 import com.Model.User;
 
 public class UserDAO {
@@ -14,18 +14,36 @@ public class UserDAO {
 	private String pass = "system";
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	
-	private static final String insertUser = "insert into user(name, gender, dob, email, address, password) values(?,?,?,?,?,?);";
-	
+	private static final String insertUser = "insert into voter(name, gender, dob, email, address, password, userCreatedDate) values(?,?,?,?,?,?,?);";
+	/*create table 
+	 * CREATE TABLE voter(voter_card_number VARCHAR(10) NOT NULL PRIMARY KEY DEFAULT '0', name VARCHAR(30) NOT NULL, gender VARCHAR(6) NOT NULL, dob VARCHAR(10) NOT NULL,email VARCHAR(30) NOT NULL, address VARCHAR(50) NOT NULL, password VARCHAR(8) NOT NULL, userCreatedDate DATETIME);
+	 */
+	/*
+	 * trigger
+	 * DELIMITER $$
+CREATE TRIGGER voter_insert
+BEFORE INSERT ON voter
+FOR EACH ROW
+BEGIN
+  INSERT INTO voter_seq VALUES (NULL);
+  SET NEW.voter_card_number = CONCAT('FVA', LPAD(LAST_INSERT_ID(), 3, '0'));
+END$$
+DELIMITER ;
+	 */
 	protected Connection getConnection() {
 		Connection conn = null;
 		try {
 			Class.forName(driver);
 			conn = DriverManager.getConnection(url, uname, pass);
-			System.out.println("connected to db.");
 		}catch(SQLException e) {
 			e.printStackTrace();	
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		}
+		if(conn!=null) {
+			System.out.println("connected to db.");
+		}else {
+			System.out.println("not connected to db.");
 		}
 		return conn;	
 	}
@@ -40,8 +58,13 @@ public class UserDAO {
 			ps.setString(4,user.getEmail());
 			ps.setString(5,user.getAddress());
 			ps.setString(6, user.getPassword());
+			
+			Date date1 = new Date(System.currentTimeMillis());
+			ps.setDate(7, date1);
+			
 			System.out.println(ps);
-			ps.executeQuery();		
+			
+			ps.executeUpdate();		
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
