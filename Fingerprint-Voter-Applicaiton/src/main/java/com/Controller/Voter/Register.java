e07f15be25e3635e39b8b890ebe5be4e59f1c1cc
91913 <91913@JaTaDhariShiv>
2023-05-27 17:47:27 +0530

    com.Controller.Voter.Register servlet added

---
+++ Fingerprint-Voter-Applicaiton/src/main/java/com/Controller/Voter/Register.java
@@ -0,0 +1,76 @@
+package com.Controller.Voter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.Dao.UserDAO;
+import com.Model.User;
+
+/**
+ * Servlet implementation class Register
+ */
+
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private UserDAO userDao = new UserDAO();
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+		String dob = request.getParameter("dob");
+		String email = request.getParameter("email");
+		String address = request.getParameter("address");
+		
+		
+		User user = new User();
+		user.setName(name);
+		user.setGender(gender);
+		user.setDob(dob);
+		user.setEmail(email);
+		user.setAddress(address);
+		
+		try {
+			userDao.insertUser(user);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect("#");
+		
+	}
+
+}
