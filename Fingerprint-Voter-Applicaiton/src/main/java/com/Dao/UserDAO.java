74726ada46911ecd860f2b4d94beff3495ece315
91913 <91913@JaTaDhariShiv>
2023-05-28 13:54:44 +0530

    comment removed

--- Fingerprint-Voter-Applicaiton/src/main/java/com/Dao/UserDAO.java
+++ Fingerprint-Voter-Applicaiton/src/main/java/com/Dao/UserDAO.java
@@ -20,18 +20,6 @@ public class UserDAO {
 
 	private static final String insertUser = "insert into voter(name, gender, dob, email, address, password, userCreatedDate) values(?,?,?,?,?,?,?);";
 
-	/*
-	 * create table CREATE TABLE voter(voter_card_number VARCHAR(10) NOT NULL
-	 * PRIMARY KEY DEFAULT '0', name VARCHAR(30) NOT NULL, gender VARCHAR(6) NOT
-	 * NULL, dob VARCHAR(10) NOT NULL,email VARCHAR(30) NOT NULL, address
-	 * VARCHAR(50) NOT NULL, password VARCHAR(8) NOT NULL, userCreatedDate
-	 * DATETIME);
-	 */
-	/*
-	 * trigger DELIMITER $$ CREATE TRIGGER voter_insert BEFORE INSERT ON voter FOR
-	 * EACH ROW BEGIN INSERT INTO voter_seq VALUES (NULL); SET NEW.voter_card_number
-	 * = CONCAT('FVA', LPAD(LAST_INSERT_ID(), 3, '0')); END$$ DELIMITER ;
-	 */
 	protected static Connection getConnection() {
 		Connection conn = null;
 		try {
