1287a21b92508098785f8a66834a4f4ca7feb4b6
91913 <91913@JaTaDhariShiv>
2023-05-28 14:31:25 +0530

    contact table query added

--- Fingerprint-Voter-Applicaiton/database/database of FVA.sql
+++ Fingerprint-Voter-Applicaiton/database/database of FVA.sql
@@ -7,4 +7,5 @@
 	CREATE TABLE voter(voter_card_number VARCHAR(10) NOT NULL PRIMARY KEY DEFAULT '0', name VARCHAR(30) NOT NULL, gender VARCHAR(6) NOT NULL, dob VARCHAR(10) NOT NULL,email VARCHAR(30) NOT NULL, address VARCHAR(50) NOT NULL, password VARCHAR(8) NOT NULL, userCreatedDate VARCHAR(20) NOT NULL);
 	-- trigger creating for auto increment primary key of voter table
     DELIMITER $$CREATE TRIGGER voter_insert BEFORE INSERT ON voter FOR EACH ROW BEGIN   INSERT INTO voter_seq VALUES (NULL);   SET NEW.voter_card_number = CONCAT('FVA', LPAD(LAST_INSERT_ID(), 7,'0')); END$$ DELIMITER ;
-
+	-- contact table create
+    CREATE TABLE contact(name VARCHAR(30) NOT NULL,email VARCHAR(30) NOT NULL,message VARCHAR(60) NOT NULL);
