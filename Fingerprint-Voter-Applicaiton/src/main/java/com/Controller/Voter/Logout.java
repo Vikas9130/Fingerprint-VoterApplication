30d905db4b5c4bd91a1e0fb4459794a6be323266
91913 <91913@192.168.1.204>
2023-06-04 20:53:17 +0530

    logout module added for voter

---
+++ Fingerprint-Voter-Applicaiton/src/main/java/com/Controller/Voter/Logout.java
@@ -0,0 +1,57 @@
+package com.Controller.Voter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Logout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+        session.invalidate();
+
+        // to expire a cookie
+        Cookie c = new Cookie("uname", "");
+        response.addCookie(c);
+        Cookie[] c1 = request.getCookies();
+        c1[0].setMaxAge(0);
+
+        response.sendRedirect("home.jsp");
+        }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
