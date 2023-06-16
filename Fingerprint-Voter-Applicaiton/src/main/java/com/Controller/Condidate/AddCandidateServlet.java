77ec9987c65b5128d9776ba1d41099bf9807bb44
91913 <91913@JaTaDhariShiv>
2023-06-16 16:07:26 +0530

    add candidate module modified

--- Fingerprint-Voter-Applicaiton/src/main/java/com/Controller/Condidate/AddCandidateServlet.java
+++ Fingerprint-Voter-Applicaiton/src/main/java/com/Controller/Condidate/AddCandidateServlet.java
@@ -17,7 +17,9 @@ import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Servlet implementation class AddCandidateServlet
@@ -59,26 +61,46 @@ public class AddCandidateServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 
+		
 		HttpSession session = request.getSession();
 		// Extract form data
 	      String name = request.getParameter("name");
 	      String gender = request.getParameter("gender");
+	      String birthdate = request.getParameter("birthdate");	      
 	      String address = request.getParameter("address");
+	      String aadhaar = request.getParameter("aadhaar");	    
 	      Part photoPart = request.getPart("photo");
+	      String partyName = request.getParameter("partyName");
 	      
 	      // Get the filename and file data from the photoPart
 	      String photoFileName = Paths.get(photoPart.getSubmittedFileName()).getFileName().toString();
 	      InputStream photoInputStream = photoPart.getInputStream();
-
+	      try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+			
 	      // Store data into the database
-	      try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
-	         String query = "INSERT INTO candidate (candidate_name, candidate_gender, candidate_address, candidate_photo) VALUES (?, ?, ?, ?)";
+	     Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+	         String query = "INSERT INTO candidate (candidate_name, candidate_gender, candidate_birth, candidate_address, candidate_aadhar, candidate_photo, party_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+	         ResultSet resultSet = null;
+	         Statement stmt = conn.createStatement();
+	         String sql = "SELECT party_id FROM party WHERE party_name = '" + partyName + "'";
+	         resultSet = stmt.executeQuery(sql);
+	         int partyId=0;
+	         if (resultSet.next()) {
+	        	    partyId = resultSet.getInt("party_id");
+	        	    // Use the partyId as needed
+	        	} else {
+	        	    // Handle the case when no party with the given name is found
+	        	}
+	         
 	         PreparedStatement statement = conn.prepareStatement(query);
 	         statement.setString(1, name);
 	         statement.setString(2, gender);
-	         statement.setString(3, address);
-	         statement.setString(4, photoFileName);
+	         statement.setString(3, birthdate);
+	         statement.setString(4, address);
+	         statement.setString(5, aadhaar);
+	         statement.setString(6, photoFileName);
+	         statement.setInt(7, partyId);
 	         statement.executeUpdate();
 	         
 	         // Save the photo file to a location or process it as per your requirement
@@ -90,7 +112,7 @@ public class AddCandidateServlet extends HttpServlet {
 	            
 	         // Redirect to a success page or display a success message
 	         response.sendRedirect("addCandidateSuccess.jsp");
-	      } catch (SQLException e) {
+	      } catch (SQLException | ClassNotFoundException e) {
 	         e.printStackTrace();
 	         // Redirect to an error page or display an error message
 	         response.sendRedirect("error.jsp");
